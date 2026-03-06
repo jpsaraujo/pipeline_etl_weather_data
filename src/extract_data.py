f72ed1e609fe234bc0cdc849ae81cc175489b318
jpsaraujo <jpedro_s@live.com>
2026-03-05 23:14:02 -0300

    criando arquivo extract_data.py

---
+++ src/extract_data.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+from pathlib import Path
+
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+api_key='3c97702b3ba4e6e298635984ab75d712'
+url = f'https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&units=metric&appid={api_key}'
+
+def extract_weather_data(url:str)-> list:
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code !=200:
+        logging.error("Erro na requisição")
+        return[]
+    
+    if not data:
+        logging.warning("Nenhum dado retornado")
+        return[]
+    
+
+
+    output_path = 'data/weather_data.json'
+    output_dir = Path(output_path).parent
+    output_dir.mkdir(parents=True,exist_ok=True)
+
+
+    with open(output_path,'w') as f:
+        json.dump(data, f,indent=4)
+
+
+    logging.info(f"Arquivo salvo em {output_path} ")
+
+    return data
+
+extract_weather_data(url)
